1d8f41e49d4502f0901e3f8bb61dde0f2ec7502c
Rev <rkeveyan23@gmail.com>
2023-01-21 17:11:53 +0400

    esim

--- reg/src/component/header/index.jsx
+++ reg/src/component/header/index.jsx
@@ -1,5 +1,4 @@
 import Nkar from '../../assets/n.jpg'
-import { useState } from 'react'
 import './style.scss'
 import {MyContext} from  "../../page/Home"
 import { useContext } from "react";
@@ -15,7 +14,7 @@ export const Header = () =>{
         <div className="header_img">
         <img src={Nkar} alt="Nkar" />
         </div>
-        
+
             {profile.btn ? <div>
             <h2>{profile.firstName} {profile.lastName}</h2>
             <p>{profile.position}</p>
